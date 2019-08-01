cf9ed3f553204d07f8a9eb802681c3528d7e4b4b
YWS66 <724549390@qq.com>
2019-08-01 22:35:09 +0800

    init gshop

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+/*
+vuex最核心的管理对象store
+需要4个配置选项，就是4个模块
+*/
+import Vue from 'vue'
+import Vuex from 'vuex'
+import state from './state'
+import actions from './actions'
+import getters from './getters'
+import mutations from './mutations'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state,
+  actions,
+  getters,
+  mutations
+})
